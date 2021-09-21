f6e5d7a2618181c68ca010305b01f1057bea2ec3
Kawser Ahmed Shovon <ahmedshovoncse13@gmail.com>
2021-09-21 17:30:30 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter></Counter>
+      <ExternalUsers></ExternalUsers>
     </div>
   );
+}
+
+const Counter = () => {
+  const [state, setstate] = useState(0)
+  const increaseHandler = () => setstate(state + 1)
+  const decreaseHandler = () => setstate(state - 1)
+  return (
+    <div>
+      <h3>Count: {state}</h3>
+      <button onClick={increaseHandler}>Increase</button>
+      <button onClick={decreaseHandler}>Decrease</button>
+    </div>
+  )
+}
+
+const ExternalUsers = () => {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+  return (
+    <div>
+      <h1>USERS</h1>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+
+const User = (props) => {
+  const style = {
+    backgroundColor: '#d3d6db',
+    margin: '10px',
+    padding: '10px'
+  }
+  return (
+    <div style={style}>
+      <h2>Name: {props.name}</h2>
+      <h3>email: {props.email}</h3>
+    </div>
+  )
 }
 
 export default App;
